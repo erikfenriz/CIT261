2ae441f4c282e63fb75589bac48adfd28eead876
Erik Rybalkin <erikfenriz@gmail.com>
2018-01-27 01:39:29 +0200

    fixed the input/output value bug

--- scripts/variables.js
+++ scripts/variables.js
@@ -2,6 +2,12 @@ var d = document,
     inputValue = d.getElementById("inputValue"),
     liveOutput = d.getElementById("liveOutput");
 
+
 inputValue.onkeyup = function () {
-    liveOutput.textContent = this.value;
+    if(liveOutput.textContent.length < 30) {
+        liveOutput.textContent = this.value;
+    }else{
+        this.value = 0;
+        liveOutput.textContent = " ";
+    }
 };
