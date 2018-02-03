3b578aba6bd6cfd612a0cdb0589de924ab554d9b
Erik Rybalkin <erikfenriz@gmail.com>
2018-02-03 16:30:55 +0200

    added TOPIC3git add -A! :fire:

---
+++ scripts/topic3/json-api.js
@@ -0,0 +1,29 @@
+
+(function () {
+    fetch('http://content.guardianapis.com/search?api-key=test')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var data = data.response.results;
+            var ul = document.createElement("ul");
+            for (var i = 0; i < data.length; i++) {
+                var textNode = document.createTextNode(data[i].webTitle);
+                var li = document.createElement("li");
+                li.appendChild(textNode);
+                ul.appendChild(li);
+            }
+            console.log(ul);
+            addNewsIntoDOM(ul)
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+})();
+
+function addNewsIntoDOM(news) {
+    var list = document.getElementById("news");
+    list.appendChild(news);
+}
