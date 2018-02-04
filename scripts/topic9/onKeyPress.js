43a5250e70ed3199d94886de187f3c333219b39e
Erik Rybalkin <erikfenriz@gmail.com>
2018-02-05 00:48:23 +0200

    added topic 4

---
+++ scripts/topic9/onKeyPress.js
@@ -0,0 +1,18 @@
+function playing(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(audio);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+window.addEventListener("keydown", playing);
+
+function removeTransition(e) {
+if (e.propertyName !== "transform") return;
+this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
